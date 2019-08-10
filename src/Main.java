53127464e2a5073c11870804133f8d3487b69439
ThunderingWest4 <ThunderingWest4@github.com>
2019-08-09 20:33:53 -0400

    Summary (required)
    Description

--- src/Main.java
+++ src/Main.java
@@ -2,10 +2,12 @@ import java.util.ArrayList;
 
 public class Main {
 
+    public void keyPressed(KeyEvent evt);
+
     public static void main(String[] args) {
         //launch(args);
 
-
+        //public void keyPressed(KeyEvent evt);
         Snake s = new Snake();
 
 
@@ -15,56 +17,35 @@ public class Main {
         int XDim = 30;
         int YDim = 30;
 
+        boolean Running = true;
+
         //useful link: https://howtodoinjava.com/java/collections/arraylist/arraylist-get-method-example/
         //obj to int https://stackoverflow.com/questions/3661413/how-to-cast-an-object-to-an-int
         //https://stackoverflow.com/questions/16956720/how-to-create-an-2d-arraylist-in-java
-        int[][] AppleGrid = new int[XDim][YDim];
 
         //30x30 grid of possible things
         //set them all to zero - binary; 0 if no apple, 1 if apple
-        int ColCount = 1;
-        int RowCount = 1;
 
         env E = new env();
 
+        ArrayList AppleGrid = E.GetAppleArray(XDim, YDim);
+
         //move to mainloop after key recieved
 
-        ArrayList appleloc = E.GetApple(XDim, YDim);
 
-        int appleX = (int) appleloc.get(0);
-
-        int appleY = (int) appleloc.get(1);
-
-        for (int i = 0; i < XDim; i++) {
-
-            for (int j = 0; j < YDim; j++) {
-
-                //format is x, y and in this i, j
-                if (i == appleX && j == appleY) {
-
-                    AppleGrid[i][j] = 1;
-
-                }
-                else {
-
-                    AppleGrid[i][j] = 0;
-                }
+        while (Running == true) {
 
 
+
+            try {
+                s.Move(1, 2, 'u');
+            } catch (ArithmeticException a) {
+
+                System.out.println("invalid direction type given to the snake");
             }
 
+
         }
-
-
-
-        try {
-            s.Move(1, 2, 'u');
-        }
-        catch (ArithmeticException a) {
-
-            System.out.println("invalid direction type given to the snake");
-        }
-
 
     }
 
--- src/env.java
+++ src/env.java
@@ -20,4 +20,45 @@ public class env {
         return a;
     }
 
+    public ArrayList GetAppleArray(int XDim, int YDim) {
+
+
+
+        ArrayList a = new ArrayList();
+        ArrayList appleloc = GetApple(XDim, YDim);
+        int[][] AppleGrid = new int[XDim][YDim];
+
+
+        int appleX = (int) appleloc.get(0);
+
+        int appleY = (int) appleloc.get(1);
+
+        for (int i = 0; i < XDim; i++) {
+
+            for (int j = 0; j < YDim; j++) {
+
+                //format is x, y and in this i, j
+                if (i == appleX && j == appleY) {
+
+                    AppleGrid[i][j] = 1;
+
+                }
+                else {
+
+                    AppleGrid[i][j] = 0;
+                }
+
+
+            }
+
+        }
+
+
+        return a;
+    }
+
+    public void IfAppleEaten(int appleX, int appleY) {
+
+    }
+
 }
