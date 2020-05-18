c8590bb0b4f9878d8f239c0b283b148e6613acfb
ThunderingWest4 <ThunderingWest4@github.com>
2020-05-18 12:12:20 -0400

    working algo, build exe for it w pyinstaller

--- AStarSnek.py
+++ AStarSnek.py
@@ -7,7 +7,7 @@ pygame.init()
 pygame.font.init()
 
 MAX = 20
-res = (MAX**2 + 2*MAX, MAX**2 + 2*MAX)
+res = (MAX**2 + 5*MAX, MAX**2 + 2*MAX)
 Goal = ()
 def GoalInit():
     return (random.randint(0, (MAX - 1)), random.randint(0, (MAX - 1)))
@@ -37,8 +37,8 @@ run = True
 #Manhattan Distance –
 
 #It is nothing but the sum of absolute values of differences in the goal’s x and y coordinates and the headent cell’s x and y coordinates respectively, i.e.,
-# h = abs (headent_cell.x – goal.x) + 
-#     abs (headent_cell.y – goal.y) 
+# h = abs (head_cell.x – goal.x) + 
+#     abs (head_cell.y – goal.y) 
 
 targetfps = 60
  
@@ -48,6 +48,8 @@ head = Start
 time.sleep(5)
 snekBody = [head]
 size = 1
+Score = 0
+pygame.display.set_caption("Score: " + str(Score))
 
 while(run == True):
     #head = ()
@@ -70,13 +72,16 @@ while(run == True):
 
         c += 1
     for g in snekBody:
-        pygame.draw.rect(screen, Blue, pygame.Rect((g[0]*(MAX+1)), (g[1]*(MAX+1)), MAX, MAX))
+        try:
+            pygame.draw.rect(screen, Blue, pygame.Rect((g[0]*(MAX+1)), (g[1]*(MAX+1)), MAX, MAX))
+        except:
+            print("Snake has been cornered. Game Over. Score: " + str(Score))
     pygame.draw.rect(screen, HeadCol, pygame.Rect((snekBody[len(snekBody)-1][0]*(MAX+1)), (snekBody[len(snekBody)-1][1]*(MAX+1)), MAX, MAX))
 
     if(not (head == goal)):
         #basically if we're not at the goal
         headH = abs(head[0] - goal[0]) + abs(head[1] - goal[1])
-        print(headH)
+        #print(headH)
         adjoining = []
         
         if((head[1] + 1) < MAX): adjoining.append((head[0], head[1] + 1)) #the block *above*
@@ -85,40 +90,40 @@ while(run == True):
         if((head[0] - 1) >= 0): adjoining.append((head[0] - 1, head[1])) #the block to the *left*
         HDict = {}
         for obj in adjoining:
-            if(obj in snekBody):
-                adjoining.remove(obj)
-            else: 
-                HDict[obj] = (abs(obj[0] - goal[0])**2 + abs(obj[1] - goal[1])**2) 
-                #Heuristics
-                HDict[obj] += abs(Start[0]-obj[0]) + abs(Start[1]-obj[1])
-                #Cost/Dist from Initial pt
+            HDict[obj] = (abs(obj[0] - goal[0])**2 + abs(obj[1] - goal[1])**2) #+ abs(Start[0]-obj[0]) + abs(Start[1]-obj[1])
+                #Heuristics + Cost
 
         #sort Heuristic Array by val
         #thanks @ https://www.saltycrane.com/blog/2007/09/how-to-sort-python-dictionary-by-keys/
         getH = 0
-        SmallestH = head
+        SmallestH = ()
+        print("Heuristic Options for Snake: %s" % HDict)
         for key, value in sorted(HDict.items(), key=lambda item: item[1]):
-            print("%s: %s" % (key, value))
-            if(getH == 0):
+            #print("%s: %s" % (key, value))
+            if(getH == 0 and (not (key in snekBody))):
                 #SmallestH = HDict[i]
                 SmallestH = key
+                getH += 1
             if(goal == key):
                 SmallestH = key
-            getH += 1
+            
 
         #print(HDict)
 
-        print(SmallestH, goal, Goal)
+        #print(SmallestH, goal, Goal)
         
         #SmallestH is now the idealplace
         #Grid[SmallestH[1]][SmallestH[0]] = 's'
         head = SmallestH
         snekBody.append(SmallestH)
         snekBody.remove(snekBody[0])
-        print(snekBody)
+        #print(snekBody)
 
     else:
         print("Apple Reached")
+        Score += 1
+        pygame.display.set_caption("Score: " + str(Score))
+        print("Score: " + str(Score))
         goalFound = False
         while(not goalFound):
             Goal = GoalInit()
@@ -135,6 +140,7 @@ while(run == True):
         snekBody.append(head)
         #time.sleep(3)
         #time.sleep(100)
+
     for event in pygame.event.get():
         if(event.type == pygame.KEYDOWN):
             if(event.key == pygame.key.Q):
